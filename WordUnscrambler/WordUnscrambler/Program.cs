d0929dfefad206aacf5ff66a89b844da41effbf0
jonboland <jon.boland@gmail.com>
2021-07-13 10:33:01 +0100

    Added WordUnscrambler

---
+++ WordUnscrambler/WordUnscrambler/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WordUnscrambler.Data;
+using WordUnscrambler.Workers;
+
+namespace WordUnscrambler
+{
+    class Program
+    {
+        private static readonly FileReader _fileReader = new FileReader();
+        private static readonly WordMatcher _wordMatcher = new WordMatcher();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string continueUnscrambling;
+                do
+                {
+                    Console.Write(Constants.WordUnscramblingOptions);
+                    var option = Console.ReadLine() ?? string.Empty;
+
+                    switch (option.ToUpper())
+                    {
+                        case Constants.File:
+                            Console.Write(Constants.EnterScrambledWordsViaFile);
+                            ProcessScrambedWordsFile();
+                            break;
+                        case Constants.Manual:
+                            Console.Write(Constants.EnterScrambledWordsManually);
+                            ProcessScrambledWordsText();
+                            break;
+                        default:
+                            Console.WriteLine(Constants.EnterScrambledWordsOptionNotRecognised);
+                            break;
+                    }
+
+                    var continueOptions = new List<string> { Constants.Yes, Constants.No };
+
+                    do
+                    {
+                        Console.Write(Constants.ContinueOptions);
+                        continueUnscrambling = Console.ReadLine().ToUpper();
+
+                    } while (!continueOptions.Contains(continueUnscrambling));
+
+                } while (continueUnscrambling == Constants.Yes);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(Constants.ErrorProgramWillBeTerminated + ex.Message);
+            }
+            
+        }
+
+        private static void ProcessScrambledWordsText()
+        {
+            var manualInput = Console.ReadLine() ?? string.Empty;
+            string[] scrambledWords = manualInput.Split(',');
+            DisplayUnscrambledWords(scrambledWords);
+        }
+
+        private static void ProcessScrambedWordsFile()
+        {
+            try
+            {
+                var fileName = Console.ReadLine() ?? string.Empty;
+                string[] scrambledWords = _fileReader.Read(fileName);
+                DisplayUnscrambledWords(scrambledWords);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Set up exception logging
+                Console.WriteLine(Constants.ErrorScrambledWordsCannotBeLoaded + ex.Message);
+            }
+        }
+
+
+        private static void DisplayUnscrambledWords(string[] scrambledWords)
+        {
+            string[] wordList = _fileReader.Read(Constants.WordListFileName);
+
+            List<MatchedWord> matchedWords = _wordMatcher.Match(scrambledWords, wordList);
+            Console.WriteLine(matchedWords);
+
+            if (matchedWords.Any())
+            {
+                foreach(var matchedWord in matchedWords)
+                {
+                    Console.WriteLine(Constants.MatchFound, matchedWord.ScrambledWord, matchedWord.Word);
+                }
+            }
+            else
+            {
+                Console.WriteLine(Constants.MatchNotFound);
+            }
+        }
+    }
+}
---
+++ WordUnscrambler/WordUnscrambler/Workers/FileReader.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace WordUnscrambler.Workers
+{
+    class FileReader
+    {
+        public string[] Read(string fileName)
+        {
+            return File.ReadAllLines(fileName);
+        }
+    }
+}
